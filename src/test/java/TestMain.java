67ccfb4af9d088fd75bae8e9778fa3e915dc9592
Brosha <smileanton@yandex.ru>
2024-12-23 22:32:36 +0300

    Initial commit

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,11 @@
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+    @Test
+    void testCalculate(){
+        Assertions.assertEquals(5, Main.calculate(3,2));
+    }
+}
